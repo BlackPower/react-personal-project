8c44361b60351b7763898c5f4535cb7af1971871
Sergey Kurilchik <sergey.kurilchik@gmail.com>
2019-02-23 22:32:15 +0200

    Добавил базовую разметру

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -1,15 +1,48 @@
 // Core
 import React, { Component } from 'react';
 
+import Spinner from 'components/Spinner';
+import Checkbox from 'theme/assets/Checkbox';
 // Instruments
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
 export default class Scheduler extends Component {
+
     render () {
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <Spinner />
+                <main>
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input
+                            placeholder = 'Поиск'
+                            type = 'search'
+                            value = ''
+                        />
+                    </header>
+                    <section>
+                        <form>
+                            <input
+                                className = { Styles.createTask }
+                                maxLength = '50'
+                                placeholder = 'Описaние моей новой задачи'
+                                type = 'text'
+                                value = ''
+                            />
+                            <button>Добавить задачу</button>
+                        </form>
+                        <div className = { Styles.overlay } />
+                    </section>
+
+                    <footer>
+                        <Checkbox />
+                        <span className = { Styles.completeAllTasks }>
+                            Все задачи выполнены
+                        </span>
+                    </footer>
+                </main>
             </section>
         );
     }
